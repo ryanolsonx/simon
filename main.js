8354bd5068b34d8d2ae9b989aa366b2f6cf7ae76
Ryan Olson <rolson@aumni.fund>
2022-11-04 10:31:02 -0600

    Revert back

--- main.js
+++ main.js
@@ -1,182 +1,181 @@
-// -- CONSTANTS
-
 const TOTAL_LEVELS = 5;
-const COLORS = ["red", "green", "blue", "yellow"];
+const TIME_BETWEEN_TILE_FLASHES = 600;
+const COLORS = ['red', 'green', 'blue', 'yellow'];
+const ONE_SECOND = 1000;
 
 const initialState = {
-  heading: "Simon Game",
-  info: "",
-  level: 0,
-  showStartButton: true,
-  canClickColorTiles: false,
-  computerColorsToGuess: [],
-  playerGuesses: [],
+	heading: 'Simon Game',
+	info: '',
+	level: 0,
+	showStartButton: true,
+	allowTileClicks: false,
+	sequence: [], // computer sequence to guess
+	humanSequence: [], // human guesses so far in sequence
 };
 
 let state = initialState;
 
 const update = (stateChanges) => {
-  state = { ...state, ...stateChanges };
-  view();
+	state = { ...state, ...stateChanges };
+	view();
 };
 
 const $ = {
-  heading: document.querySelector("#heading"),
-  tiles: document.querySelector("#tiles"),
-  info: document.querySelector("#info"),
-  startBtn: document.querySelector("#start-button"),
+	heading: document.querySelector('#heading'),
+	tiles: document.querySelector('#tiles'),
+	info: document.querySelector('#info'),
+	startBtn: document.querySelector('#start-button'),
 
-  getTileByColor: (color) => document.querySelector(`[data-tile='${color}']`),
-  getSoundByColor: (color) => document.querySelector(`[data-sound='${color}']`),
+	getTileByColor: (color) => document.querySelector(`[data-tile='${color}']`),
+	getSoundByColor: (color) => document.querySelector(`[data-sound='${color}']`),
 };
 
 const activateTile = (color) =>
-  $.getTileByColor(color).classList.add("activated");
+	$.getTileByColor(color).classList.add('activated');
 const deactivateTile = (color) =>
-  $.getTileByColor(color).classList.remove("activated");
+	$.getTileByColor(color).classList.remove('activated');
 
 const view = () => {
-  $.heading.textContent = state.heading;
+	$.heading.textContent = state.heading;
 
-  if (state.info) {
-    $.info.classList.remove("hidden");
-    $.info.textContent = state.info;
-  } else {
-    $.info.classList.add("hidden");
-  }
+	if (state.info) {
+		$.info.classList.remove('hidden');
+		$.info.textContent = state.info;
+	} else {
+		$.info.classList.add('hidden');
+	}
 
-  // activate / deactivate tiles
-  if (state.activatedColor) {
-    COLORS.filter((color) => color !== state.activatedColor).forEach(
-      deactivateTile
-    );
-    activateTile(state.activatedColor);
-  } else {
-    COLORS.forEach(deactivateTile);
-  }
+	// activate / deactivate tiles
+	if (state.activatedColor) {
+		COLORS.filter((color) => color !== state.activatedColor).forEach(
+			deactivateTile
+		);
+		activateTile(state.activatedColor);
+	} else {
+		COLORS.forEach(deactivateTile);
+	}
 
-  if (state.showStartButton) {
-    $.startBtn.classList.remove("hidden");
-  } else {
-    $.startBtn.classList.add("hidden");
-  }
+	if (state.showStartButton) {
+		$.startBtn.classList.remove('hidden');
+	} else {
+		$.startBtn.classList.add('hidden');
+	}
 
-  if (state.canClickColorTiles) {
-    $.tiles.classList.remove("unclickable");
-  } else {
-    $.tiles.classList.add("unclickable");
-  }
+	if (state.allowTileClicks) {
+		$.tiles.classList.remove('unclickable');
+	} else {
+		$.tiles.classList.add('unclickable');
+	}
 };
 
 const resetGame = () => update(initialState);
 
 const handleColorClicked = async (color) => {
-  update({ playerGuesses: [...state.playerGuesses, color] });
+	update({ humanSequence: [...state.humanSequence, color] });
 
-  const index = state.playerGuesses.length - 1;
+	const index = state.humanSequence.length - 1;
 
-  $.getSoundByColor(color).play();
+	$.getSoundByColor(color).play();
 
-  const remainingGuesses =
-    state.computerColorsToGuess.length - state.playerGuesses.length;
-  const guess = state.playerGuesses[index];
-  const actual = state.computerColorsToGuess[index];
+	const remainingGuesses = state.sequence.length - state.humanSequence.length;
+	const guess = state.humanSequence[index];
+	const actual = state.sequence[index];
 
-  const isWrong = guess !== actual;
+	const isWrong = guess !== actual;
 
-  if (isWrong) {
-    alert("Sorry, game over.");
-    resetGame();
-    return;
-  }
+	if (isWrong) {
+		alert('Sorry, game over.');
+		resetGame();
+		return;
+	}
 
-  const answeredFullSequenceCorrectly =
-    state.playerGuesses.length === state.computerColorsToGuess.length;
-  const hasFinishedAllLevels = state.level === TOTAL_LEVELS;
-  const hasFinishedRound = answeredFullSequenceCorrectly;
+	const answeredFullSequenceCorrectly =
+		state.humanSequence.length === state.sequence.length;
+	const hasFinishedAllLevels = state.level === TOTAL_LEVELS;
+	const hasFinishedRound = answeredFullSequenceCorrectly;
 
-  const hasWon = hasFinishedRound && hasFinishedAllLevels;
+	const hasWon = hasFinishedRound && hasFinishedAllLevels;
 
-  if (hasWon) {
-    // show that there are no guesses left..
-    update({
-      info: getRemainingGuessesMessage(0),
-    });
-    alert("You won!");
-    resetGame();
-    return;
-  } else if (hasFinishedRound) {
-    update({
-      playerGuesses: [],
-      info: "Success! Keep going!",
-    });
+	if (hasWon) {
+		// show that there are no guesses left..
+		update({
+			info: getRemainingGuessesMessage(0),
+		});
+		alert('You won!');
+		resetGame();
+		return;
+	} else if (hasFinishedRound) {
+		update({
+			humanSequence: [],
+			info: 'Success! Keep going!',
+		});
 
-    await sleep(1000);
+		await sleep(ONE_SECOND);
 
-    nextRound();
-  } else {
-    // still guessing...
-    update({
-      info: getRemainingGuessesMessage(remainingGuesses),
-    });
-  }
+		nextRound();
+	} else {
+		// still guessing...
+		update({
+			info: getRemainingGuessesMessage(remainingGuesses),
+		});
+	}
 };
 
-$.tiles.addEventListener("click", (event) => {
-  const { tile } = event.target.dataset;
+$.tiles.addEventListener('click', (event) => {
+	const { tile } = event.target.dataset;
 
-  if (tile) {
-    handleColorClicked(tile);
-  }
+	if (tile) {
+		handleColorClicked(tile);
+	}
 });
 
 const getRemainingGuessesMessage = (remaining) =>
-  `Your turn. click ${remaining} tile(s).`;
+	`Your turn. click ${remaining} tile(s).`;
 
 const allowHumanToGuess = () => {
-  update({
-    info: getRemainingGuessesMessage(state.level),
-    canClickColorTiles: true,
-  });
+	update({
+		info: getRemainingGuessesMessage(state.level),
+		allowTileClicks: true,
+	});
 };
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const computerShowSequence = async (seq) => {
-  for (const color of seq) {
-    $.getSoundByColor(color).play();
-    update({ activatedColor: color });
-    await sleep(600); // Keep activiated for a bit
-    update({ activatedColor: "" });
-    await sleep(50);
-  }
+	for (const color of seq) {
+		$.getSoundByColor(color).play();
+		update({ activatedColor: color });
+		await sleep(TIME_BETWEEN_TILE_FLASHES);
+		update({ activatedColor: '' });
+		await sleep(50);
+	}
 };
 
 const getRandomColor = () => COLORS[Math.floor(Math.random() * COLORS.length)];
 
 const nextRound = async () => {
-  const nextLevel = state.level + 1;
+	const level = state.level + 1;
 
-  update({
-    heading: `Level ${nextLevel} of ${TOTAL_LEVELS}`,
-    level: nextLevel,
-    canClickColorTiles: false,
-    info: "Wait for the computer",
-  });
+	update({
+		heading: `Level ${level} of ${TOTAL_LEVELS}`,
+		level: level,
+		allowTileClicks: false,
+		info: 'Wait for the computer',
+	});
 
-  const nextSequence = [...state.computerColorsToGuess, getRandomColor()];
-  await computerShowSequence(nextSequence);
+	const nextSequence = [...state.sequence, getRandomColor()];
+	await computerShowSequence(nextSequence);
 
-  update({ sequence: nextSequence });
+	update({ sequence: nextSequence });
 
-  await sleep(1000);
-  allowHumanToGuess();
+	await sleep(ONE_SECOND);
+	allowHumanToGuess();
 };
 
 const startGame = () => {
-  update({ showStartButton: false });
+	update({ showStartButton: false });
 
-  nextRound();
+	nextRound();
 };
 
-$.startBtn.addEventListener("click", startGame);
+$.startBtn.addEventListener('click', startGame);
